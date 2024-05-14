2cc085c754f8fd5d778118a51cf59a1720d6ea98
Sonal Kawale <89601070+Sonal0128@users.noreply.github.com>
2024-05-15 02:11:52 +0530

    Add files via upload

---
+++ src/main/java/com/Mypack/FullstackBackend1Application.java
@@ -0,0 +1,16 @@
+package com.Mypack;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FullstackBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackend1Application.class, args);
+	}
+
+}
